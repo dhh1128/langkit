52b61a48c8e869a2b2fbe5d121fb11e91e1c5c18
Daniel Hardman <daniel.hardman@gmail.com>
2024-04-20 17:54:52 +0200

    working, improved version of glossary.find()
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- langkit/glossary.py
+++ langkit/glossary.py
@@ -25,8 +25,24 @@ class DefnItem:
         else:
             self.kind = EXACT_EQUIV
             self.value = txt.lstrip()
+
+    @property
+    def gloss(self):
+        i = self.value.find('(')
+        return self.value if i == -1 else self.value[:i].rstrip()
+    
+    @property
+    def explanation(self):
+        i = self.value.find('(')
+        if i > -1:
+            j = self.value.find(')', i + 1)
+            if j == -1: j = len(self.value)
+            return self.value[i+1:j]
+        return ''
+    
     def __str__(self):
         return self.kind + self.value
+
     def __lt__(self, other):
         if self.kind == EXACT_EQUIV:
             if other.kind == EXACT_EQUIV:
@@ -43,6 +59,7 @@ class Defn:
     def __init__(self, txt):
         self.equivs = []
         self.parse(txt)
+        self._txt = None
 
     def parse(self, txt):
         while txt:
@@ -57,59 +74,10 @@ class Defn:
         self.equivs.sort()
 
     def __str__(self):
-        return ' / '.join([str(e) for e in self.equivs])
+        if self._txt is None:
+            self._txt = ' / '.join([str(e) for e in self.equivs])
+        return self._txt
     
-class MatchExpr:
-    def __init__(self, expr):
-        self.expr = expr
-        i = expr.find('*')
-        j = expr.find('?')
-        if i > -1:
-            if j > -1:
-                self._i = min(i, j)
-            else:
-                self._i = i
-        elif j > -1:
-            self._i = j
-        else:
-            self._i = -1
-        self._regex = None if self._i == -1 else re.compile(expr.replace('?', '.').replace('*', '.*?'))
-
-    @property
-    def wildcarded(self):
-        return self._i > -1
-
-    @property
-    def starter(self) -> str:
-        return self.expr if self._i == -1 else self.expr[:self._i]
-
-    def matches(self, txt) -> bool:
-        return self._regex.match(txt) if self._regex else (self.expr == txt)
-
-SCOPED_SEARCH_EXPR = re.compile(r'(?:^|\s)(l(?:e(?:x)?)?|p(?:o(?:s)?)?|d(?:e(?:f(?:n)?)?)?|n(?:o(?:t(?:e(?:s)?)?)?)?):')
-class SearchExpr:
-    def __init__(self, expr):
-        criteria = []
-        chunks = SCOPED_SEARCH_EXPR.split(expr)
-        # Accept text without a field prefix, and pretend it had one.
-        if ':' not in chunks[0]: chunks.insert(0, 'lex:')
-        i = 0
-        while i < len(chunks):
-            m = chunks[i + 1].strip()
-            if m:
-                criteria.append(chunks[i][0], MatchExpr(m))
-            i += 2
-        
-    def matches(self, entry: Entry) -> bool:
-        for field_selector, match_expr in self.criteria:
-            if field_selector == 'l': text = entry.lexeme
-            elif field_selector == 'p': text = entry.pos
-            elif field_selector == 'd': text = str(entry.defn)
-            else: text = entry.notes
-            if not match_expr.matches(text):
-                return False
-        return True
-
 class Entry:
     def __init__(self, fields):
         if isinstance(fields, str):
@@ -135,6 +103,108 @@ def _is_header(entry: Entry) -> bool:
 _DIVIDER_LEX_PAT = re.compile('-{2,}$')
 def _is_divider(entry: Entry) -> bool:
     return _DIVIDER_LEX_PAT.match(entry.lexeme)
+
+class MatchExpr:
+    def __init__(self, expr):
+        self.expr = expr
+        i = expr.find('*')
+        j = expr.find('?')
+        if i > -1:
+            if j > -1:
+                self._i = min(i, j)
+            else:
+                self._i = i
+        elif j > -1:
+            self._i = j
+        else:
+            self._i = -1
+        self._regex = None if self._i == -1 else re.compile(expr.replace('?', '.').replace('*', '.*?'))
+
+    @property
+    def wildcarded(self):
+        return self._i > -1
+    
+    @property
+    def first_wildcard_index(self):
+        return self._i
+
+    @property
+    def starter(self) -> str:
+        return self.expr if self._i == -1 else self.expr[:self._i]
+    
+    def __str__(self):
+        return self.expr
+
+    def matches(self, txt) -> bool:
+        return self._regex.match(txt) if self._regex else (self.expr == txt)
+
+SCOPED_SEARCH_EXPR = re.compile(r'(?:^|\s)(l(?:e(?:x)?)?|p(?:o(?:s)?)?|d(?:e(?:f(?:n)?)?)?|n(?:o(?:t(?:e(?:s)?)?)?)?):')
+class SearchExpr:
+    def __init__(self, expr):
+        criteria = []
+        chunks = SCOPED_SEARCH_EXPR.split(expr)
+        # Accept text without a field prefix. The meaning of this is that
+        # the user wants to search in both the lexeme and the definition,
+        # unless one of those other fields is specified explicitly.
+        if ':' not in chunks[0]:
+            first_chars = [x[0] for x in chunks if ':' in x]
+            if 'l' in first_chars:
+                if 'd' in first_chars:
+                    raise ValueError("Bad search syntax.")
+                chunks.insert(0, 'd:')
+            elif 'd' in first_chars:
+                chunks.insert(0, 'l')
+            else:
+                chunks.insert(0, 'x')
+            
+        i = 0
+        while i < len(chunks):
+            m = chunks[i + 1].strip()
+            if m:
+                criteria.append((chunks[i][0], MatchExpr(m)))
+            i += 2
+        self.criteria = criteria
+
+    def fuzzify(self):
+        changed = False
+        for i in range(len(self.criteria)):
+            field = self.criteria[i][0]
+            if field != 'p':
+                matcher = self.criteria[i][1]
+                expr = matcher.expr
+                if matcher.first_wildcard_index != 0:
+                    expr = '*' + expr
+                if expr[-1] not in '*?':
+                    expr = expr + '*'
+                expr = expr.replace(' ', '*')
+                if expr != matcher.expr:
+                    changed = True
+                    self.criteria[i] = (field, MatchExpr(expr))
+        return changed
+    
+    def __str__(self):
+        return ' '.join([f"{f}:{m}" for f, m in self.criteria])
+
+    @property
+    def starter(self):
+        for field, matcher in self.criteria:
+            if field == 'l':
+                return matcher.starter
+        return ''
+        
+    def matches(self, entry: Entry) -> bool:
+        for field_selector, match_expr in self.criteria:
+            if field_selector == 'l': text = entry.lexeme
+            elif field_selector == 'p': text = entry.pos
+            elif field_selector == 'd': text = str(entry.defn)
+            elif field_selector == 'x':
+                if not match_expr.matches(entry.lexeme) and not match_expr.matches(str(entry.defn)):
+                    return False
+                continue
+            else: text = entry.notes
+            if not match_expr.matches(text):
+                return False
+        return True
 
 class Glossary:
     def __init__(self):
@@ -250,21 +320,27 @@ class Glossary:
 
     def find(self, expr, max_hits=5, exclude=None, try_fuzzy=True):
         hits = []
-        s_expr = SearchExpr(expr)
+        s_expr = expr if isinstance(expr, SearchExpr) else SearchExpr(expr)
         initial_search = s_expr.starter
         index = bisect.bisect_left(self.entries, initial_search, key=lambda x: x.lexeme) \
             if initial_search else 0
         while index < self.lexeme_count and max_hits != 0:
             entry = self.entries[index]
-            if s_expr.matches(entry.lexeme):
+            if s_expr.matches(entry):
                 if not exclude or (entry not in exclude):
                     hits.append(entry)
                     max_hits -= 1
+            elif initial_search and entry.lexeme > s_expr.starter:
+                break
             index += 1
         # Now that we've found hits that start with expr, look
-        # for ones that just contain it.
-        if try_fuzzy and max_hits and not expr.startswith('*'):
-            hits += self.find_lexeme('*' + expr, max_hits, hits)
+        # for ones that just contain it. The purpose of always
+        # doing a fuzzy search is not simply to make finding easier,
+        # but to make sure that as glossary edits occur, an awareness
+        # of similar words is encouraged.
+        if try_fuzzy and max_hits:
+            if s_expr.fuzzify():
+                hits += self.find(s_expr, max_hits, hits)
 
         return hits
     
--- langkit/tests/glossary_test.py
+++ langkit/tests/glossary_test.py
@@ -13,6 +13,53 @@ MD_PRE = "# A glossary\n\na paragraph of text\n* some stuff\n* some more stuff\n
 MD_POST = "# A section after glossary\nsome stuff after a glossary"
 
 
+def test_MatchExpr():
+    def assert_me(text, starter, fwi):
+        me = MatchExpr(text)
+        assert me.starter == starter
+        assert me.first_wildcard_index == fwi
+        assert bool(fwi > -1) == me.wildcarded
+    assert_me("abc", "abc", -1)
+    assert_me("ab*c", "ab", 2)
+    assert_me("ab?c", "ab", 2)
+    assert_me("*abc", "", 0)
+    assert_me("?abc", "", 0)
+
+def test_searchexpr():
+    entry = Entry(("abc", "123", "def xyz", "notes"))
+    def assert_matches(expr):
+        se = SearchExpr(expr)
+        assert se.matches(entry)
+    assert_matches("lex:abc")
+    assert_matches("l:abc")
+    assert_matches("pos:123")
+    assert_matches("defn:*xyz")
+    assert_matches("not:notes")
+    assert_matches("abc*")
+
+def test_searchexpr_starter():
+    def assert_starter(expr, starter):
+        se = SearchExpr(expr)
+        assert se.starter == starter
+    assert_starter("lex:abc pos:v", "abc")
+    assert_starter("pos:v lex:ab*c", "ab")
+    assert_starter("def:abc pos:v", "")
+    assert_starter("lex:*abc notes:notes pos:v", "")
+    
+def test_searchexpr_fuzzify():
+    def assert_fuzzy(expr, fuzzy_equiv):
+        se = SearchExpr(expr)
+        original = str(se)
+        changed = se.fuzzify()
+        assert str(se) == fuzzy_equiv
+        assert changed == (original != fuzzy_equiv)
+    assert_fuzzy("lex:abc", "l:*abc*")
+    assert_fuzzy("pos:abc", "p:abc")
+    assert_fuzzy("notes:?abc", "n:?abc*")
+    assert_fuzzy("defn:?abc?", "d:?abc?")
+    assert_fuzzy("defn:abc xyz", "d:*abc*xyz*")
+
+
 def assert_di(x, kind, txt=None):
     di = DefnItem(x)
     assert di.kind == kind
@@ -45,6 +92,16 @@ def test_DefnItem_sort():
         assert items[0].kind == EXACT_EQUIV
         for j in range(len(EQUIV_CHARS)):
             assert items[1 + j].kind == EQUIV_CHARS[j]
+
+def test_DefnItem_gloss_and_explanation():
+    def assert_gex(item, gloss, explanation):
+        di = DefnItem(item)
+        assert di.gloss == gloss
+        assert di.explanation == explanation
+    assert_gex("abc", "abc", '')
+    assert_gex("abc (xyz)", "abc", 'xyz')
+    assert_gex("abc (xyz", "abc", 'xyz')
+    assert_gex("abc xyz)", "abc xyz)", '')
 
 def test_Defn_multi():
     defn = Defn('~def/ <a/> bc / :something/ another something')
@@ -83,19 +140,25 @@ def test_save_markdown():
     assert txt.find(MD_POST) > -1
     assert txt.find("sweet | a | ~having a sugary flavor") > -1
 
-def test_find_lexeme_simple():
-    assert g.find_lexeme('fry')
-    assert not g.find_lexeme('not-there')
+def test_find_simple():
+    assert g.find('l:fry')
+    assert not g.find('l:not-there')
+    assert not g.find('d:fry')
+    assert g.find('d:to cook*')
 
 def test_find_lexeme_wildcards():
-    assert g.find_lexeme('fr*')
-    assert g.find_lexeme('?ry')
-    assert g.find_lexeme('*ry')
-    assert not g.find_lexeme('*not*')
+    assert g.find('l:fr*')
+    assert g.find('l:?ry')
+    assert g.find('l:*ry')
+    assert not g.find('l:*not*')
 
 def test_find_defn_simple():
-    assert g.find_defn('a yellow fruit')
-    assert not g.find_defn('purple vegetable')
+    assert g.find('d:a yellow fruit')
+    assert not g.find('d:purple vegetable')
 
 def test_find_defn_wildcards():
-    assert len(g.find_defn('*fruit')) > 1
+    assert len(g.find('d:*fruit')) > 1
+
+def test_find_anywhere():
+    assert len(g.find('swallow')) == 1
+    assert len(g.find('vinegar')) == 1
